14795ec29ed6f73c27de178f88d5e1a04a309592
Sam <samuellutz75@gmail.com>
2025-09-03 16:11:15 -0400

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CrossroadsWebgame/', // 👈 important!
+  base: '/CrossroadsWebgame/', // 👈 must match repo name
 })
